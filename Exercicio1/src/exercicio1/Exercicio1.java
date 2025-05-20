e2fa2168d88e81bb5011fd0f5bd1615d8e18d924
MariTrindadee <mari.florestb@gmail.com>
2025-05-20 14:46:56 -0300

    add do exercicio 1

---
+++ Exercicio1/src/exercicio1/Exercicio1.java
@@ -0,0 +1,26 @@
+
+package exercicio1;
+
+
+
+  public class Exercicio1 {
+
+    public static boolean ehNumeroPerfeito(int numero) {
+        if (numero <= 1) return false;
+
+        int somaDivisores = 0;
+
+        for (int i = 1; i <= numero / 2; i++) {
+            if (numero % i == 0) {
+                somaDivisores += i;
+            }
+        }
+
+        return somaDivisores == numero;
+    }
+
+    public static void main(String[] args) {
+        int numero = 6;
+        System.out.println(numero + " é perfeito? " + ehNumeroPerfeito(numero));
+    }
+}
